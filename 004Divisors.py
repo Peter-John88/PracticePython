e20f3200e1dcb1601effccb2cda2a3108e978b9a
Peter-John88 <bailing.guo@gmail.com>
2018-05-03 19:00:05 +0800

    Create 004Divisors.py

---
+++ 004Divisors.py
@@ -0,0 +1,41 @@
+# -*-  coding:utf-8  -*-
+
+"""
+
+Exercise 4
+
+Create a program that asks the user for a number and then prints out a list of all the divisors of that number. (If you don’t know what a divisor is, it is a number that divides evenly into another number. For example, 13 is a divisor of 26 because 26 / 13 has no remainder.)
+
+"""
+
+# my solution
+
+num = int(raw_input("Enter a num:"))
+for i in range(1, num):
+    if num % i == 0:
+        print i
+
+
+
+
+# standard solution
+
+__author__ = 'jeffreyhunt'
+
+num = int(input("Please choose a number to divide: "))
+
+listRange = list(range(1,num+1))
+
+divisorList = []
+
+for number in listRange:
+    if num % number == 0:
+        divisorList.append(number)
+
+print(divisorList)
+
+
+
+
+
+
